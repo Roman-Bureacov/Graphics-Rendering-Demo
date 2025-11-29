91c3fdbe511207ba63c38d65dc5dfe529e15d57d
RomaBureacov <RomanBureacov@gmail.com>
2025-11-28 18:39:16 -0800

    appended matrix inversion to the matrix math library

--- app/model/javaGL/matrix/MatrixMath.java
+++ app/model/javaGL/matrix/MatrixMath.java
@@ -1,5 +1,7 @@
 package model.javaGL.matrix;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * A utility class that performs matrix algebra.
  *
@@ -92,6 +94,12 @@ public final class MatrixMath {
         return lResult;
     }
 
+    /**
+     * Performs matrix addition if and only if the left and right matrices are the same dimenions.
+     * @param pLeft the left matrix
+     * @param pRight the right matrix
+     * @return the resulting matrix from the addition
+     */
     public static Matrix<Double> addMatrix(final Matrix<Double> pLeft, final Matrix<Double> pRight) {
         final int lRows = pLeft.rowCount();
         final int lCols = pLeft.columnCount();
@@ -110,5 +118,120 @@ public final class MatrixMath {
         }
 
         return lResult;
+    }
+
+    /**
+     * Inverses the matrix if the matrix is invertible.
+     * @param pMatrix the matrix to inverse
+     * @return the resulting inverted matrix, or null if the matrix is non-invertible
+     */
+    @Nullable
+    public static Matrix<Double> inverse(final Matrix<Double> pMatrix) {
+        /*
+        Linear Algebra with Applications
+        Steven J. Leon and Lisette de Pillis
+
+        Chapter 2.3, page 116
+
+        A^-1 = 1/det(A) * adj(A) when det(A) != 0
+
+        where the adjoint of a matrix is, the transpose of the matrix of, the cofactor matrix of each term
+         */
+
+        final int lRows = pMatrix.rowCount();
+        final int lCols = pMatrix.columnCount();
+
+        if (lRows != lCols) return null;
+
+        final Matrix<Double> lInverse = new DoubleMatrix(lRows, lCols);
+
+        // find adjoint matrix
+        for (int row = 0; row < lRows; row++) {
+            for (int col = 0; col < lCols; col++) {
+                // transpose in advance
+                lInverse.set(col, row, cofactor(pMatrix, col, row));
+            }
+        }
+
+        // find determinant
+        // we can make use of the fact that the inverse matrix at this moment
+        // if made up of cofactors
+        double lDeterminant = 0;
+        for (int col = 0; col < lCols; col++) {
+            lDeterminant += pMatrix.get(0, col) * lInverse.get(0, col);
+        }
+
+        if (lDeterminant == 0) return null;
+
+        for (int row = 0; row < lRows; row++) {
+            for (int col = 0; col < lCols; col++) {
+                lInverse.set(row, col, 1d / lDeterminant * lInverse.get(row, col));
+            }
+        }
+
+        return lInverse;
+    }
+
+    /**
+     * Finds the cofactor for the matrix at the row and column
+     * @param pMatrix the matrix to look in
+     * @param pRow the row
+     * @param pCol the column
+     * @return the cofactor of the row and column
+     */
+    public static double cofactor(final Matrix<Double> pMatrix, final int pRow, final int pCol) {
+        if (pMatrix.rowCount() != pMatrix.columnCount()) 
+            throw new IllegalArgumentException("Matrix must be square to find cofactor");
+        
+        // get minor
+        final int lRows = pMatrix.rowCount();
+        final int lCols = pMatrix.columnCount();
+        final Matrix<Double> lMinor = new DoubleMatrix(lRows - 1, lCols - 1);
+        for (int row = 0; row < lRows; row++) {
+            if (row == pRow) {
+                // skip row
+                row++;
+            } else {
+                for (int col = 0; col < lCols; col++) {
+                    if (col == pCol) {
+                        // skip column
+                        col++;
+                    } else {
+                        lMinor.set(row, col, pMatrix.get(row, col));
+                    }
+                }
+            }
+        }
+
+        return Math.pow(-1, pRow + pCol) * determinant(lMinor);
+    }
+
+    /**
+     * Finds the determinants for the matrix at the row and column
+     * @param pMatrix the matrix to look in
+     * @return the determinant of the row and column
+     */
+    public static double determinant(final Matrix<Double> pMatrix) {
+        final int lRows = pMatrix.rowCount();
+        final int lCols = pMatrix.columnCount();
+
+        if (lRows != lCols)
+            throw new IllegalArgumentException("Matrix must be square to find determinant");
+        
+        double lDet = 0;
+
+        if (lRows == 1) {
+            return pMatrix.get(0, 0);
+        } else if (lRows == 2) {
+            return pMatrix.get(0, 0) * pMatrix.get(1, 1)
+                    - pMatrix.get(0, 1) * pMatrix.get(1, 0);
+        } else {
+            // find the determinant using the first row
+            for (int col = 0; col < lCols; col++) {
+                lDet = pMatrix.get(0, col) * cofactor(pMatrix, 0, col);
+            }
+        }
+
+        return lDet;
     }
 }
